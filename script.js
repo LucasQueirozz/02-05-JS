ea566734201134288b224835301b5bc17bc8c27c
LucasQueirozz <131412268+LucasQueirozz@users.noreply.github.com>
2023-05-03 20:35:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Entrada de dados
+let ladoQuadrado = Number(prompt("Digite o comprimento do lado do quadrado"));
+
+//Processamento de dados
+let area = ladoQuadrado ** 2;
+//let area = Math.pow(ladoQuadrado, 2);
+let mensagem = `A área do quadrado é ${area}`;
+
+//Saída de dados
+alert(mensagem);
